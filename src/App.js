67f63842e9f027d76e76db579c1875e22d8c78ac
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-08-09 15:39:14 -0700

    Add nav links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Section from './Section';
+import _ from 'lodash';
 
 // determines what sections to load and their order
 var sectionIds = [
@@ -10,8 +11,28 @@ var sectionIds = [
   'scale',
 ];
 
+var navWidth = 200;
+var padding = 20;
 class App extends Component {
   render() {
+    var navStyle = {
+      width: navWidth,
+      padding,
+      position: 'fixed',
+      borderRight: '1px solid',
+    };
+    var contentStyle = {
+      marginLeft: navWidth + 2 * padding,
+      padding,
+    };
+
+    var navLinks = sectionIds.map(id => {
+      return (
+        <div>
+          <a href={'#' + id}>{_.capitalize(id)}</a>
+        </div>
+      )
+    });
     var sections = sectionIds.map(id => {
       var text = require('./sections/' + id + '.md.js');
       text = text && text.default;
@@ -20,7 +41,12 @@ class App extends Component {
     });
     return (
       <div className="App">
-        {sections}
+        <div id='nav' style={navStyle}>
+          {navLinks}
+        </div>
+        <div id='content' style={contentStyle}>
+          {sections}
+        </div>
       </div>
     );
   }
--- src/Section.js
+++ src/Section.js
@@ -5,9 +5,9 @@ var md = new Remarkable({linkTarget: '_new', html: true});
 class Section extends Component {
   render() {
     var rawMarkup = { __html: md.render(this.props.text)};
-    
+
     return (
-      <div className="Section">
+      <div className="Section" id={this.props.id}>
         <div dangerouslySetInnerHTML={rawMarkup} />
       </div>
     );
