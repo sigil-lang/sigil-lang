bcb2114b978f7deb34352e60164f5cb340ecde67
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-08-09 15:23:14 -0700

    Load and display all sections in order

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Section from './Section';
 
-import atom from './sections/atom.md.js';
+// determines what sections to load and their order
+var sectionIds = [
+  'atom',
+  'compound',
+  'aggregate',
+  'graph',
+  'scale',
+];
+
 class App extends Component {
   render() {
+    var sections = sectionIds.map(id => {
+      var text = require('./sections/' + id + '.md.js');
+      text = text && text.default;
+
+      return (<Section key={id} id={id} text={text} />)
+    });
     return (
       <div className="App">
-        <Section text={atom} />
+        {sections}
       </div>
     );
   }
