cdfb4e28c3620cffd85447f1fba2d01bebf8f8f3
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-09-15 18:50:42 -0700

    Atom relationship italicize

--- src/sections/atom-relationship.md.js
+++ src/sections/atom-relationship.md.js
@@ -22,8 +22,8 @@ A relationship should be represented by a **line**. The default relationship has
   * At large scale (above 1 hem): relative line thickness can be useful and can be chosen by the user. Thickness then represents a numeric value, thin lines being low values and the thicker the line, the higher the value. As visual advice, don’t create lines thinner than 1px or thicker than the radius of the (smallest) node. For example, for all relationships between “Emma Watson” and the different movies she ACTED_IN, the line thickness could represent her salary in each movie.
   * At small scale (below 0.5 hem): relative line thickness becomes difficult to distinguish, when the radius of the nodes becomes close to the overall minimum of 1 pixel, and is therefore not recommended
 
-* Line color: Can be chosen by the user, but shouldn’t be used to indicate categorical attributes, such as the type/role of the relationships. Or any numerical value, in that case, use line thickness instead. Color can however be used for highlighting, dimming, and tracing paths. Arrowhead color and fill should be the same as the line stroke.
+* Line color: Can be chosen by the user, but shouldn’t be used to indicate categorical attributes, such as the type/role of the relationships. Or any numerical value, in that case, use line thickness instead. Color *can* however be used for highlighting, dimming, and tracing paths. Arrowhead color and fill should be the same as the line stroke.
 
 ### Self relationship
-This relationship connects the same node together. For example, the relationship of FAVORITE_ACTRESS from the node “Emma Watson” could be “Emma Watson” herself. 
+This relationship connects the same node together. For example, the relationship of FAVORITE_ACTRESS from the node “Emma Watson” could be “Emma Watson” herself.
 `;
