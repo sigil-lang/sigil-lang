29ca75ef0da62f607854ac0d7a6fa75796eca900
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-09-15 18:38:38 -0700

    Edit line breaks in atom-node.md.js

--- src/sections/atom-node.md.js
+++ src/sections/atom-node.md.js
@@ -1,5 +1,4 @@
 export default `
-
 ## Node
 
 A single data record, with 0 or more labels, and 0 or more properties.  Labels are a special type of property that groups the node. For example, a node can be a single data record with property name=“Emma Watson” and labels “Actress” and “British”. Another node can be a single data record with property title=“Beauty and the Beast” and label “Movie”.
@@ -18,6 +17,7 @@ A node should be represented by a **circle**. The default node has a light grey 
   ![Atomic node stroke color at small and large scale](${require('../images/atom-node-2.png')})
 
 * Stroke style (note: stroke is only used in larger scale graphs where the nodes are big enough to have strokes at all):
+
   <img src=${require('../images/atom-node-3.png')} alt='Atomic node stroke style with or without properties' width=300px />
   * 0 properties: dashed
   * 1 or more properties: solid
